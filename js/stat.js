d7ef9421c4d35af9f043f25ff8afeb02f5e15908
Aleksei Piatetskyi <a.piatetskyi@gmail.com>
2018-01-29 17:50:33 +0200

    Добавил вывод статистики

---
+++ js/stat.js
@@ -0,0 +1,60 @@
+'use strict';
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 20;
+var COL_WIDTH = 40;
+var COL_HEIGHT = 150;
+var COL_GAP = 50;
+var COL_COLOR = 'rgba(255, 0, 0, 1)';
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var renderCenteredText = function (ctx, text, coordinateY) {
+  var textWidth = ctx.measureText(text).width;
+  var coordinateX = CLOUD_X + (CLOUD_WIDTH - textWidth) / 2;
+
+  ctx.fillText(text, coordinateX, coordinateY);
+};
+
+var getMaxValue = function (times) {
+  return times.reduce(function (prev, current) {
+    return Math.max(prev, current);
+  }, 0);
+};
+
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, .7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.font = '16px PT Mono';
+  renderCenteredText(ctx, 'Ура вы победили!', 36);
+  renderCenteredText(ctx, 'Список результатов:', 54);
+
+  var maxTime = getMaxValue(times);
+  var firstColX = CLOUD_X + (CLOUD_WIDTH - COL_WIDTH * names.length - COL_GAP * (names.length - 1)) / 2;
+  var colHeights = times.map(function (time) {
+    return (COL_HEIGHT * time) / maxTime;
+  });
+  var colYs = colHeights.map(function (col) {
+    return CLOUD_HEIGHT + CLOUD_Y - GAP - FONT_GAP - col;
+  });
+
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = COL_COLOR;
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255,' + (Math.random() + 0.1).toFixed(1) + ')';
+    }
+    ctx.fillRect(firstColX + (COL_WIDTH + COL_GAP) * i, colYs[i], COL_WIDTH, colHeights[i]);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillText(names[i], firstColX + (COL_WIDTH + COL_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), firstColX + (COL_WIDTH + COL_GAP) * i, colYs[i] - GAP);
+  }
+};
